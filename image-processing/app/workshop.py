bb141ec76afb7e97d757d4250b6fb94c1c65c6d3
soyantonio-w <63258919+soyantonio-w@users.noreply.github.com>
2021-03-29 09:57:02 -0600

    Add process server (#9)
    * remove packages to simplify workshop
    
    * remove non used code
    
    * add mocked server
    
    * add pika to publish into rabbitmq
    
    * refactor: use not found to encapsulate logic
    
    * feat: add direct exchange
    
    * feat: add direct exchange to processing service
    
    * docs: add curl examples

---
+++ image-processing/app/workshop.py
@@ -0,0 +1,23 @@
+from json import dumps
+from typing import Dict
+from app.config import open_connection
+
+
+class PublishManager:
+    def __init__(self, exchange: str, routing_key: str):
+        self.__exchange = exchange
+        self.__routing_key = routing_key
+        self.__connection = open_connection()
+        self.__channel = self.__connection.channel()
+
+    def publish(self, payload: Dict[str, str]):
+        self.__declare_exchange('direct')
+        self.__channel.basic_publish(exchange=self.__exchange,
+                                     routing_key=self.__routing_key,
+                                     body=dumps(payload))
+
+    def __declare_exchange(self, exchange_type: str):
+        self.__channel.exchange_declare(exchange=self.__exchange, exchange_type=exchange_type)
+
+    def close_connection(self):
+        self.__connection.close()
